f014d220f12491236cd10343fb0c796c73dec403
Uiseop Eom <ytrtef@gmail.com>
2023-06-17 00:35:52 +0900

    Update setup_dotfile.sh

--- setup_dotfile.sh
+++ setup_dotfile.sh
@@ -11,9 +11,9 @@ mkdir -p .dotfile-backup
 dotfile checkout
 if [ $? = 0 ]; then
   echo "Checked out dotfile.";
-  else
-    echo "Backing up pre-existing dot files.";
-    dotfile checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfile-backup/{}
+else
+  echo "Backing up pre-existing dot files.";
+  dotfile checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfile-backup/{}
 fi;
 dotfile checkout
 dotfile dotfile status.showUntrackedFiles no
