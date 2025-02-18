60657b391998c550934a74acf540529836cf9cd7
Uiseop Eom <ytrtef@gmail.com>
2025-02-18 22:59:42 +0900

    Update install-node.sh

--- scripts/installations/install-node.sh
+++ scripts/installations/install-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PROFILE=/dev/null
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | /bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
-nvm install 18
+nvm install 20
 echo -e "Current node version: \033[0;31m$(node -v)\033[0m"
